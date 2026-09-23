26d43cb2763bd0fe9eccc07e2e867ef1566d2f2d
rajabboyegamov4-ui <rajabboyegamov4@gmail.com>
2026-09-23 15:17:59 +0500

    Update security.py

--- security.py
+++ security.py
@@ -1,13 +1,10 @@
 from passlib.context import CryptContext
 
-# Parolni shifrlash uchun bcrypt konteksti
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# Bcrypt o'rniga xavfsiz va xatolarsiz pbkdf2_sha256 kontekstiga o'tamiz
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
-    # Bcrypt cheklovi (72 bayt) uchun parolni qirqib olamiz
-    password_truncated = password.encode('utf-8')[:72].decode('utf-8', errors='ignore')
-    return pwd_context.hash(password_truncated)
+    return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    password_truncated = plain_password.encode('utf-8')[:72].decode('utf-8', errors='ignore')
-    return pwd_context.verify(password_truncated, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
